96f0c9dc1c8dec9b9e260f8bce28e2565a383d16
ahmadfatan <ahmad.fatan.tif423@polban.ac.id>
2024-03-19 21:44:57 +0700

    adding main

---
+++ main.py
@@ -0,0 +1,16 @@
+print("=============================")
+print("WELCOME TO MONEY TRACKER APP")
+print("=============================\n")
+
+print("Select an Option : \n")
+print("1. Enter new Expenses/Budget\n")
+print("2. View Expenses/Budget Summary")
+
+choise = int(input())
+
+if choise == 1 :
+    pass
+elif choise == 2:
+    pass
+else :
+    exit()
